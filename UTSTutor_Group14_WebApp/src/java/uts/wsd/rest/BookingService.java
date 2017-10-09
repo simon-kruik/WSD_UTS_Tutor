1411b9eff06072cead15fdda2ac9500ac0e2e0c2
Dallyfish <31730214+Dallyfish@users.noreply.github.com>
2017-10-09 19:05:41 +1100

    Booking Application/Booking Service Beta
    Still workin on it

--- UTSTutor_Group14_WebApp/src/java/uts/wsd/rest/BookingService.java
+++ UTSTutor_Group14_WebApp/src/java/uts/wsd/rest/BookingService.java
@@ -5,8 +5,12 @@
  */
 package uts.wsd.rest;
 
+import javax.servlet.ServletContext;
 import javax.ws.rs.*;
 import javax.ws.rs.core.*;
+import javax.xml.bind.JAXBException;
+import java.io.*;
+import uts.wsd.*;
 
 /**
  *
@@ -15,13 +19,41 @@ import javax.ws.rs.core.*;
 @Path("/booking")
 public class BookingService {
 
-    
+    @Context
+    private ServletContext application;
+
+    private BookingsApplication getBookingsApp() throws JAXBException, IOException {
+        synchronized (application) {
+            BookingsApplication bookingsApp = (BookingsApplication) application.getAttribute("bookingsApp");
+            if (bookingsApp == null) {
+                bookingsApp = new BookingsApplication();
+                bookingsApp.setBookingsFilePath(application.getRealPath("WEB-INF/bookings.xml"));
+                application.setAttribute("bookingsApp", bookingsApp);
+            }
+            return bookingsApp;
+        }
+    }
+
     // Test method for REST //
     @Path("test")
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String test() {
         return "If you are reading this, REST is working";
+    }
+
+    @Path("bookings")
+    @GET
+    @Produces(MediaType.APPLICATION_XML)
+    public Bookings getBookings() throws Exception {
+        return getBookingsApp().getBookings();
+    }
+
+    @Path("bookings/{bookingID}")
+    @GET
+    @Produces(MediaType.APPLICATION_XML)
+    public Booking getBooking(@PathParam("email") String email) throws Exception {
+        return getBookingsApp().getBookings().getBooking(email);
     }
 
 }
